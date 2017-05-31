e57f3fa9a9b43f2e2678a6763132f040f48e2d8d
245160801@qq.com <245160801@qq.com>
2017-05-31 16:08:24 +0800

    统一修改 init 项目名称

--- server/action/index.js
+++ server/action/index.js
@@ -2,5 +2,5 @@ var index = require('../model/index.js');
 var util = require('../lib/util.js');
 
 module.exports = function(req, res){
-    res.render('vue-yog/page/index.tpl', index.getData());
+    res.render('{{-app_name-}}/page/index.tpl', index.getData());
 };
